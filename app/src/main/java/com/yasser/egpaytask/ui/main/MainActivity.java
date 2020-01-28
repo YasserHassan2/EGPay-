ec4b6bb73d37b08d3383bc4e309c67739bc88b1a
Yasser <yasserhassan.it.asd>
2020-01-28 17:14:24 +0200

    finish UI Desgin

--- app/src/main/java/com/yasser/egpaytask/ui/main/MainActivity.java
+++ app/src/main/java/com/yasser/egpaytask/ui/main/MainActivity.java
@@ -1,30 +1,47 @@
 package com.yasser.egpaytask.ui.main;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.telecom.Call;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.Spinner;
+import android.widget.TextView;
 import android.widget.Toast;
 
-import com.yasser.egpaytask.R;
-import com.yasser.egpaytask.retrofit.APIInterface;
+import java.util.ArrayList;
 
-public class MainActivity extends AppCompatActivity implements UserView{
+
+public class MainActivity extends AppCompatActivity implements UserView {
 
     UserPresenter userPresenter;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        userPresenter = new UserPresenter(MainActivity.this, this);
+        setContentView(userPresenter.initViews(MainActivity.this));
 
-        userPresenter = new UserPresenter(MainActivity.this,this);
+
+
+
 
         Toast.makeText(this,
                 userPresenter.sendUser(), Toast.LENGTH_LONG).show();
 
 
     }
+
 
 
 }
--- app/src/main/java/com/yasser/egpaytask/ui/main/UserPresenter.java
+++ app/src/main/java/com/yasser/egpaytask/ui/main/UserPresenter.java
@@ -2,11 +2,25 @@ package com.yasser.egpaytask.ui.main;
 
 
 import android.content.Context;
+import android.content.DialogInterface;
+import android.graphics.Color;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.Spinner;
 import android.widget.Toast;
+
+import androidx.appcompat.app.AlertDialog;
 
 import com.yasser.egpaytask.Pojo.User;
 import com.yasser.egpaytask.retrofit.APIInterface;
 import com.yasser.egpaytask.retrofit.ApiUtil;
+
+import java.util.ArrayList;
 
 import retrofit2.Callback;
 
@@ -16,6 +30,8 @@ public class UserPresenter {
     APIInterface apiInterface;
     String result="";
     Context mContext;
+
+    static String spinnerValue = "";
 
     public UserPresenter(UserView userView,Context context) {
         this.mContext= context;
@@ -43,5 +59,98 @@ public class UserPresenter {
         });
         return result;
     }
+    public LinearLayout initViews(final Context mContext) {
+
+        // for VERTIVAL linearlayout
+        LinearLayout linearLayout2 = new LinearLayout(mContext);
+        linearLayout2.setBackgroundColor(Color.parseColor("#ffffff"));
+        linearLayout2.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.MATCH_PARENT));
+        linearLayout2.setOrientation(LinearLayout.VERTICAL);
+
+        EditText ed_name = new EditText(mContext);
+        Spinner spinner_required = new Spinner(mContext);
+        EditText ed_type = new EditText(mContext);
+        EditText ed_defultValue = new EditText(mContext);
+        EditText ed_Multipule = new EditText(mContext);
+        EditText ed_sort = new EditText(mContext);
+        Button btn_post = new Button(mContext);
+
+        spinner_required.setLayoutParams(new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT , ViewGroup.LayoutParams.WRAP_CONTENT));
+
+        final ArrayList<String> spinnerArray = new ArrayList<String>();
+        spinnerArray.add("Please Select Requird");
+        spinnerArray.add("Yes");
+        spinnerArray.add("No");
+
+        ArrayAdapter<String> spinnerArrayAdapter = new ArrayAdapter<String>(mContext, android.R.layout.simple_spinner_dropdown_item, spinnerArray);
+        spinner_required.setAdapter(spinnerArrayAdapter);
+
+        ed_name.setHint("Name");
+        ed_name.setLayoutParams(new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT , ViewGroup.LayoutParams.WRAP_CONTENT));
+
+        ed_type.setHint("Type");
+        ed_type.setLayoutParams(new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT , ViewGroup.LayoutParams.WRAP_CONTENT));
+
+        ed_defultValue.setHint("defult Value");
+        ed_defultValue.setLayoutParams(new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT , ViewGroup.LayoutParams.WRAP_CONTENT));
+
+        ed_Multipule.setHint("Multipule");
+        ed_Multipule.setLayoutParams(new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT , ViewGroup.LayoutParams.WRAP_CONTENT));
+
+        ed_sort.setHint("Sort");
+        ed_sort.setLayoutParams(new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT , ViewGroup.LayoutParams.WRAP_CONTENT));
+
+        btn_post.setLayoutParams(new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT , ViewGroup.LayoutParams.WRAP_CONTENT));
+        btn_post.setText("Post Data");
+        btn_post.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(mContext);
+                builder.setMessage("Look at this dialog!")
+                        .setCancelable(false)
+                        .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int id) {
+
+                            }
+                        });
+                AlertDialog alert = builder.create();
+                alert.show();
+            }
+        });
+
+        spinner_required.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                if (spinnerArray.get(i).equals("Yes")){
+                    Toast.makeText(mContext, "Required", Toast.LENGTH_SHORT).show();
+                }else if (spinnerArray.get(i).equals("No"))
+                {
+                    Toast.makeText(mContext, "Optional", Toast.LENGTH_SHORT).show();
+                }else
+                {
+                    Toast.makeText(mContext, "Please Select Value", Toast.LENGTH_SHORT).show();
+                    spinnerValue = "no value";
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+                Toast.makeText(mContext, "Please Select Value From Spinner", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        // add components to linearlayout
+        linearLayout2.addView(ed_name);
+        linearLayout2.addView(spinner_required);
+        linearLayout2.addView(ed_type);
+        linearLayout2.addView(ed_Multipule);
+        linearLayout2.addView(ed_defultValue);
+        linearLayout2.addView(ed_sort);
+        linearLayout2.addView(btn_post);
+
+
+        return linearLayout2;
+    }
+
 
 }
